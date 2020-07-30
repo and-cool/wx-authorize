f1728dcac00d3d5ea92c86e6eb412b7eb47d74de
jeffries <hejunosr@163.com>
2020-07-30 11:49:36 +0800

    update shutdowm shell script

--- assembly/bin/shutdown.sh
+++ assembly/bin/shutdown.sh
@@ -17,7 +17,7 @@ ARGS="http://127.0.0.1:8080/actuator/shutdown"
 s_pid=0
 checkPid() {
    java_ps=`jps -l | grep $APP_MAIN_CLASS`
-   if [ -n "$java_ps" ]; thensw
+   if [ -n "$java_ps" ]; then
       s_pid=`echo $java_ps | awk '{print $1}'`
    else
       s_pid=0
@@ -29,7 +29,7 @@ checkPid
 if [ $s_pid -ne 0 ]; then
     echo -e "Stopping $APP_MAIN_CLASS ...(pid=$s_pid)"
 #    nohup java -classpath $CLASSPATH $SHUTDOWN_CLASS $ARGS >./shutdown.out 2>&1 &
-    kill -2 $s_pid >./shutdown.out 2>&1 &
+    kill -9 $s_pid >./shutdown.out 2>&1 &
     sleep 5
     checkPid
     if [ $s_pid -eq 0 ]; then
